612ee318952b34b5bf7bb1cb0aa78f3e843a7ab1
Ozan Akaydin <ozanakaydin@gmail.com>
2022-11-18 18:01:30 +0300

    Cookie configuration was created

--- Asp.Net Core MVC/AspNetCoreIdentity_Authentication_Authorization/AspNetCoreIdentity_Authentication_Authorization/Program.cs
+++ Asp.Net Core MVC/AspNetCoreIdentity_Authentication_Authorization/AspNetCoreIdentity_Authentication_Authorization/Program.cs
@@ -3,6 +3,8 @@ using AspNetCoreIdentity_Authentication_Authorization.Data;
 using AspNetCoreIdentity_Authentication_Authorization.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
+using SameSiteMode = Microsoft.AspNetCore.Http.SameSiteMode;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,7 +20,7 @@ builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
     options.User.RequireUniqueEmail = true; // Email benzersiz olmali
     options.User.AllowedUserNameCharacters = "abcdefghijklmnoqprstuvwxyzABCDEFGHIJKLMNOQPRSTUVWXYZ0123456789-._@+"; // Kullanicinin girebilecegi karakterler
     options.Password.RequiredLength = 6; // Sifre en az 6 karakter olmali
-    options.Password.RequiredUniqueChars = 0; 
+    options.Password.RequiredUniqueChars = 0;
     options.Password.RequireNonAlphanumeric = false;
     options.Password.RequireDigit = false; // 0-9 ardisik sayi girilemez
     options.Password.RequireUppercase = false; // Buyuk harf kullanimi zorunlu degil
@@ -32,6 +34,27 @@ builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
 .AddErrorDescriber<CustomIdentityErrorDescriber>()
 .AddEntityFrameworkStores<AppDbContext>();
 
+// Cookie conf.
+CookieBuilder cookieBuilder = new()
+{
+    Name = "MyProject", // Browser'da cookie'nin gosterildigi deger
+    HttpOnly = false, // Sadece Http isteklerinde gosterilsin
+    Expiration = TimeSpan.FromDays(60), // Clien 60 gun boyunca username ve password girmeden siteye girebilir
+    SameSite = SameSiteMode.Lax, // Cookie kaytdedildikten sonra herhangi bir site uzerinden cookie degerine ulasilabilir.
+    //SameSite = SameSiteMode.Strict ====>  Cookie kaytdedildikten sonra sadece ilgili site uzerinden cookie degerine ulasilabilir. (Siteler arasi istek hirsizligini engeller.)
+    SecurePolicy = CookieSecurePolicy.SameAsRequest  //Browser sitenin Http yada Https istek sekline gore kullanici cookie'sini gonderir
+    //SecurePolicy = CookieSecurePolicy.Always  ====> Browser sadece Https uzerinden istek gelirse kullanici cookie'sini gonderir
+    //SecurePolicy = CookieSecurePolicy.None // Browser  Http yada Https uzerinden istek gelirse kullanici cookie'sini gonderir
+};
+
+builder.Services.ConfigureApplicationCookie(opt =>
+{
+    opt.Cookie = cookieBuilder;
+    opt.LoginPath = new PathString("/Home/Login");
+    opt.LogoutPath = new PathString("/Home/Logout");
+    opt.AccessDeniedPath = new PathString("/Home/AccessDenied");
+    opt.SlidingExpiration = true; // Cookie suresini Expiration kadar daha uzatir.
+});
 
 
 
