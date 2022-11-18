1bb3aa088b8152bf0db911781ce8dcbb7e9b341f
Ozan Akaydin <ozanakaydin@gmail.com>
2022-11-18 20:12:35 +0300

    update migration

---
+++ Asp.Net Core MVC/AspNetCoreIdentity_Authentication_Authorization/AspNetCoreIdentity_Authentication_Authorization/Migrations/20221118160432_AppDbContext_Updated.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspNetCoreIdentity_Authentication_Authorization.Migrations
+{
+    public partial class AppDbContext_Updated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
