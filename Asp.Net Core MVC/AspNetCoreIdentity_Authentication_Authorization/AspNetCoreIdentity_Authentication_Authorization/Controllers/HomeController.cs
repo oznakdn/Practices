ade49f8e5aedc7195d09ac44a3d67e4d3ce062f4
Ozan Akaydin <ozanakaydin@gmail.com>
2022-11-18 20:50:42 +0300

    Login method and view created

--- Asp.Net Core MVC/AspNetCoreIdentity_Authentication_Authorization/AspNetCoreIdentity_Authentication_Authorization/Controllers/HomeController.cs
+++ Asp.Net Core MVC/AspNetCoreIdentity_Authentication_Authorization/AspNetCoreIdentity_Authentication_Authorization/Controllers/HomeController.cs
@@ -2,19 +2,22 @@
 using AspNetCoreIdentity_Authentication_Authorization.Models.ViewModels.UserViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace AspNetCoreIdentity_Authentication_Authorization.Controllers
 {
     public class HomeController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public HomeController(UserManager<AppUser> userManager)
+        public HomeController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
-       
+
         public IActionResult Index()
         {
             return View();
@@ -28,8 +31,8 @@ namespace AspNetCoreIdentity_Authentication_Authorization.Controllers
         [HttpPost]
         public async Task<IActionResult> SignUp(SignUpViewModel model)
         {
-           
-            if(ModelState.IsValid)
+
+            if (ModelState.IsValid)
             {
                 AppUser user = new()
                 {
@@ -53,8 +56,9 @@ namespace AspNetCoreIdentity_Authentication_Authorization.Controllers
             return View(model);
         }
 
-        public IActionResult Login()
+        public IActionResult Login(string ReturnUrl)
         {
+            TempData["ReturnUrl"] = ReturnUrl;
             return View();
         }
 
@@ -62,9 +66,64 @@ namespace AspNetCoreIdentity_Authentication_Authorization.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel loginView)
         {
-           
 
-            return View();
+            if (ModelState.IsValid)
+            {
+                AppUser user = await _userManager.FindByEmailAsync(loginView.Email); // Email den kullaniciyi buluyoruz.
+
+                if (user != null) // user varsa
+                {
+
+                    if (await _userManager.IsLockedOutAsync(user)) // kullanici kilitli ise
+                    {
+                        ModelState.AddModelError("", "Your account was locked for a while, please try again");
+                        return View(loginView);
+
+                    }
+
+                    await _signInManager.SignOutAsync(); // Mecvutta cookie varsa onu siliyoruz.
+
+                    SignInResult result = await _signInManager.PasswordSignInAsync(user, loginView.Password, loginView.RememberMe, false); // Giris yapiyoruz
+
+
+
+                    if (result.Succeeded) // Giris basarili ise
+                    {
+                        await _userManager.ResetAccessFailedCountAsync(user); // basarili giris yapan kullanicinin basarisiz giris denemeleri sifirlanir
+
+                        if (TempData["ReturnUrl"] != null)
+                        {
+                            return Redirect(TempData["ReturnUrl"].ToString()); // girmek istedigi sayfaya yonlendirir
+                        }
+                        return RedirectToAction("Index", "Member");
+                    }
+                    else // Giris basarisiz ise
+                    {
+
+                        await _userManager.AccessFailedAsync(user); // basarisiz giris sayisini 1'er attirir
+                        int failLoginCount = await _userManager.GetAccessFailedCountAsync(user); // basarisiz giris sayisi
+                        ModelState.AddModelError("", $"{failLoginCount}. fail try!"); // hatali giris sayisi kullaniciya gosterilir
+
+                        if (failLoginCount == 3) // 3 basarisiz giristen sonra kullanici kilitleniyor
+                        {
+                            await _userManager.SetLockoutEndDateAsync(user, new DateTimeOffset().AddMinutes(5)); // kullanici 5 dk kilitlenir
+                            ModelState.AddModelError("", "Your account locked for 3 fail login, please try again.");
+                            return View(loginView);
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", "Email or Password is wrong.");
+                            return View(loginView);
+                        }
+                    }
+                }
+                else // user yoksa
+                {
+                    ModelState.AddModelError("", "User is no exists");
+                }
+            }
+
+            return View(loginView);
         }
 
 
